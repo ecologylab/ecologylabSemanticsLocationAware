1019e74b8e03a0a1c20c3303903376f175b8dc35
quyin <quyin@ecologylab.net>
2012-01-12 21:18:26 +0000

    commit: restructure of the following projects (moving sources into a dedicated source folder 'src'):
    ecologylabGeneratedSemantics
    ecologylabSemantics
    ecologylabSemanticsCyberneko
    ecologylabSemanticsExample
    ecologylabSemanticsLocationAware
    
    java compiler changed to generate sources to the right place.

---
+++ src/ecologylab/semantics/sensing/GisFeatures.java
@@ -0,0 +1,183 @@
+/**
+ * 
+ */
+package ecologylab.semantics.sensing;
+
+import com.drew.metadata.exif.GpsDirectory;
+
+import ecologylab.generic.Continuation;
+import ecologylab.generic.Debug;
+import ecologylab.net.ParsedURL;
+import ecologylab.semantics.collecting.SemanticsGlobalScope;
+import ecologylab.semantics.generated.library.gps.GisLocation;
+import ecologylab.semantics.generated.library.gps.PostalAddress;
+import ecologylab.semantics.generated.library.search.YahooGeoCodeResult;
+import ecologylab.semantics.generated.library.search.YahooResult;
+import ecologylab.semantics.generated.library.search.YahooResultSet;
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.builtins.Document;
+import ecologylab.semantics.metadata.builtins.DocumentClosure;
+import ecologylab.serialization.types.scalar.DoubleType;
+
+/**
+ * Tools for extracting GIS data from EXIF+ headrs from image files, 
+ * and using these to populate appropriate S.IM.PL Metadata.
+ * 
+ * @author andruid
+ */
+public class GisFeatures extends Debug
+{
+	public static final MetadataExifFeature	GPS_LATITUDE_FEATURE			= new MetadataExifFeature( "latitude", GpsDirectory.TAG_GPS_LATITUDE);
+	
+ public static final MetadataExifFeature	GPS_ALTITUDE_FEATURE			= new MetadataExifFeature( "altitude", GpsDirectory.TAG_GPS_ALTITUDE);
+
+	public static final MetadataExifFeature	GPS_LATITUDE_REF_FEATURE	= new MetadataExifFeature( "latitude_ref", GpsDirectory.TAG_GPS_LATITUDE_REF);
+
+	public static final MetadataExifFeature	GPS_LONGITUDE_REF_FEATURE	= new MetadataExifFeature( "longitude_ref", GpsDirectory.TAG_GPS_LONGITUDE_REF);
+
+	public static final MetadataExifFeature	GPS_LONGITUDE_FEATURE			= new MetadataExifFeature( "longitude", GpsDirectory.TAG_GPS_LONGITUDE);
+	
+	public static final MetadataExifFeature	GPS_IMG_DIRECTION_REF_FEATURE		= new MetadataExifFeature( "direction_ref", GpsDirectory.TAG_GPS_IMG_DIRECTION_REF);
+	
+	public static final MetadataExifFeature	GPS_IMG_DIRECTION_FEATURE		= new MetadataExifFeature( "direction", GpsDirectory.TAG_GPS_IMG_DIRECTION);
+
+	public static final MetadataExifFeature	GPS_METADATA_FEATURES[]		= {
+																																		GPS_LATITUDE_FEATURE,
+																																		GPS_LONGITUDE_FEATURE,
+																																		GPS_LONGITUDE_REF_FEATURE,
+																																		GPS_LATITUDE_REF_FEATURE,
+																																		GPS_ALTITUDE_FEATURE,
+																																		GPS_IMG_DIRECTION_FEATURE,
+																																		GPS_IMG_DIRECTION_REF_FEATURE,
+																																		new MetadataExifFeature("satellites", GpsDirectory.TAG_GPS_SATELLITES),
+																																		};
+
+	double																	latitude, longitude, altitude, direction;
+
+
+
+	GisFeatures(com.drew.metadata.Directory gpsDir, String gpsLatitudeString)
+	{
+		double converted 					= convertToDegrees(gpsLatitudeString);
+		
+		String gpsLatitudeRef 		= GPS_LATITUDE_REF_FEATURE.getStringValue(gpsDir);
+		latitude									= "S".equals(gpsLatitudeRef) ? -converted : converted;
+		
+		String gpsLongitudeString = GPS_LONGITUDE_FEATURE.getStringValue(gpsDir);
+		converted 								= convertToDegrees(gpsLongitudeString);
+	
+		String gpsLongitudeRef 		= GPS_LONGITUDE_REF_FEATURE.getStringValue(gpsDir);
+		longitude									= "W".equals(gpsLongitudeRef) ? -converted : converted;
+		
+		String altitudeString 		= GPS_ALTITUDE_FEATURE.getStringValue(gpsDir);
+		if (altitudeString != null)
+			altitude								= DoubleType.rationalToDouble(altitudeString);
+		
+		String directionString		= GPS_IMG_DIRECTION_FEATURE.getStringValue(gpsDir);
+		if (directionString != null)
+			direction								= DoubleType.rationalToDouble(directionString);
+	}
+
+	private double convertToDegrees(String stringDMS)
+	{
+		String[] dms		= stringDMS.split(" ", 3);
+
+		double degrees 	= rationalToDouble(dms, 0);
+
+		double minutes 	= rationalToDouble(dms, 1);
+
+		double seconds 	= rationalToDouble(dms, 2);
+
+		return degrees + (minutes / 60) + (seconds / 3600);
+	}
+
+	public static double rationalToDouble(String[] dms, int whichRational)
+	{
+		String rationalString = dms[whichRational];
+		return DoubleType.rationalToDouble(rationalString);
+	}
+
+	/**
+	 * Construct one of these, and use it to get nice dms GPS data from the exif metadata --
+	 * if the GPS stuff is there.
+	 * 
+	 * @param gpsDir
+	 * @return		An instance of this, or null.
+	 */
+	public static GisFeatures extract(com.drew.metadata.Directory gpsDir)
+	{
+		String gpsLatitudeString = GPS_LATITUDE_FEATURE.getStringValue(gpsDir);
+		return (gpsLatitudeString != null) ? new GisFeatures(gpsDir, gpsLatitudeString) : null;
+	}
+	
+	public static boolean containsGisMixin(final Metadata parent)
+	{
+		return parent.containsMixin(GisLocation.class);
+	}
+
+	GisLocation extractMixin(Metadata parentMetadata)
+	{
+		GisLocation result					= containsGisMixin(parentMetadata) ? parentMetadata.getMixin(GisLocation.class) : new GisLocation();
+		setMixinFeatures(result);
+
+		return result;
+	}
+
+	private void setMixinFeatures(GisLocation result)
+	{
+		result.setLongitude(longitude);
+		result.setLatitude(latitude);
+		result.setAltitude(altitude);
+		result.setDirection(direction);
+	}
+	
+	//TODO Reverse geo-code, to discover place!
+	// http://developer.yahoo.com/geo/placefinder/
+	// http://where.yahooapis.com/geocode?q=38.898717,-77.035974&gflags=R&appid=[yourappidhere]
+	public static final String	YAHOO_REVERSE_GEO	= "http://where.yahooapis.com/geocode?gflags=R&q=";
+	
+	public static Metadata extractMixin(com.drew.metadata.Directory gpsDir, final SemanticsGlobalScope semanticsScope, final Metadata parentMetadata)
+	{
+		final GisFeatures gpsFeatures				= extract(gpsDir);
+		if (gpsFeatures == null)
+			return null;
+		
+		final GisLocation result		= gpsFeatures.extractMixin(parentMetadata);	
+		parentMetadata.addMixin(result);
+		
+		ParsedURL reverseGeo	= ParsedURL.getAbsolute(YAHOO_REVERSE_GEO + result.getLongitude()+","+result.getLatitude());
+
+		Document reverseGeoDoc			= semanticsScope.getOrConstructDocument(reverseGeo);
+		DocumentClosure geoClosure	= reverseGeoDoc.getOrConstructClosure();
+		geoClosure.addContinuation(new Continuation<DocumentClosure>()
+		{
+			@Override
+			public void callback(DocumentClosure o)
+			{
+				// TODO Auto-generated method stub
+				YahooResultSet resultSet		= (YahooResultSet) o.getDocument();
+				YahooResult yahooResult = resultSet.getResults().get(0);
+				if (yahooResult instanceof YahooGeoCodeResult)
+				{
+					YahooGeoCodeResult yahooGeoResult = (YahooGeoCodeResult) yahooResult;
+					println(yahooGeoResult.getCountry() + " > " + yahooGeoResult.getCity());
+					PostalAddress postalAddress	= new PostalAddress(semanticsScope.getMetaMetadataRepository().getMMByClass(PostalAddress.class));
+					postalAddress.setStreetAddress(yahooGeoResult.getLine1());
+					postalAddress.setLocality(yahooGeoResult.getCity());
+					postalAddress.setRegion(yahooGeoResult.getState());
+					postalAddress.setPostalCode(yahooGeoResult.getPostal());
+					postalAddress.setCounty(yahooGeoResult.getCounty());
+					postalAddress.setCountry(yahooGeoResult.getCountry());
+					parentMetadata.addMixin(postalAddress);
+				}
+				else
+				{
+					gpsFeatures.warning("type of results is not YahooGeoResult! " + yahooResult);
+				}
+			}
+		});
+		geoClosure.queueDownload();
+		return result;
+	}
+	
+}
---
+++ src/ecologylab/semantics/sensing/MetadataExifFeature.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package ecologylab.semantics.sensing;
+
+import java.util.Date;
+
+import com.drew.metadata.MetadataException;
+
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.MetadataFieldDescriptor;
+import ecologylab.semantics.metadata.scalar.MetadataDate;
+import ecologylab.semantics.metadata.scalar.MetadataScalarBase;
+
+/**
+ * Maps an exif (metadata) tag with a S.IM.PL Metadata field in a S.IM.PL Metadata object.
+ * 
+ * @author andruid
+ */
+public class MetadataExifFeature
+{
+	String 	simplMetadataTag;
+	
+	int			exifTag;
+	
+	MetadataFieldDescriptor mfd;
+	
+	public MetadataExifFeature(String simplMetadataTag, int	exifTag)
+	{
+		this.simplMetadataTag	= simplMetadataTag;
+		this.exifTag					= exifTag;
+	}
+
+	/**
+	 * @return the metadataName
+	 */
+	public String getMetadataName()
+	{
+		return simplMetadataTag;
+	}
+
+	/**
+	 * @return the exifTag
+	 */
+	public int getExifTag()
+	{
+		return exifTag;
+	}
+	
+	/**
+	 * This verision of extract does it the right way, using the ScalarType derived from the
+	 * correct MetadataFieldDescriptor.
+	 * <p/>
+	 * Extract a value from the exif header. Set the matching S.IM.PL Metadata field.
+	 * 
+	 * @param metadata
+	 * @param dir
+	 * @return
+	 */
+	public Object extract(Metadata metadata, com.drew.metadata.Directory dir)
+	{
+		String value	= getStringValue(dir);
+		Object result	= null;
+		if (value != null)
+		{
+			MetadataFieldDescriptor mfd	= metadata.getFieldDescriptorByTagName(simplMetadataTag);
+			mfd.set(metadata, value);
+		}
+		return result;
+	}
+	
+	/**
+	 * Get a String type value out of the exif dir.
+	 * 
+	 * @param dir
+	 * @return
+	 */
+	public String getStringValue(com.drew.metadata.Directory dir)
+	{
+		String result	= null;
+		if (dir.containsTag(exifTag))
+			result			= dir.getString(exifTag);
+		return result;
+	}
+	/**
+	 * Probably never needed, as the extract method is more general.
+	 * 
+	 * @param metadata
+	 * @param dir
+	 * @return
+	 */
+	public Date extractDate(Metadata metadata, com.drew.metadata.Directory dir)
+	{
+		Date value	= getDateValue(dir);
+		if (value != null)
+		{
+			MetadataDate metaDate				= new MetadataDate(value);
+			setMetadataField(metadata, metaDate);
+		}
+		return value;
+	}
+
+	/**
+	 * @param metadata
+	 * @param metaDate
+	 */
+	public void setMetadataField(Metadata metadata, MetadataScalarBase metaDate)
+	{
+		MetadataFieldDescriptor mfd	= metadata.getFieldDescriptorByTagName(simplMetadataTag);
+		if (mfd != null)
+			mfd.setField(metadata, metaDate);
+	}
+	/**
+	 * Get a Date type value out of the exif dir.
+	 * 
+	 * @param dir
+	 * @return
+	 */
+	public Date getDateValue(com.drew.metadata.Directory dir)
+	{
+		Date result	= null;
+		if (dir.containsTag(exifTag))
+			try
+			{
+				result			= dir.getDate(exifTag);
+			}
+			catch (MetadataException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return result;
+	}
+}
